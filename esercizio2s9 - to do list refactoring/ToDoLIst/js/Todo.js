d0d57dc3ce5a309405db62431194d84ca7782258
DI-LEO-GIUSEPPE-GABRIELE <98736129+DI-LEO-GIUSEPPE-GABRIELE@users.noreply.github.com>
2022-02-10 10:31:01 +0100

    Delete esercizio2s9 - to do list refactoring/ToDoLIst directory

--- esercizio2s9 - to do list refactoring/ToDoLIst/js/Todo.js
+++
@@ -1,7 +0,0 @@
-export class Todo {
-    constructor(titolo, testo) {
-        this.titolo = titolo;
-        this.testo = testo;
-        this.fullTodo = () => this.titolo + ' - ' + this.testo;
-    }
-}
